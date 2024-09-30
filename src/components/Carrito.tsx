9e67d9fe13af1a7dec9921c90d6c8a3f4d7e5788
FrancoArmanasco2000 <programmingarmanasco@gmail.com>
2024-09-30 20:22:25 -0300

    Add logic to the cart

--- src/components/Carrito.tsx
+++ src/components/Carrito.tsx
@@ -1,3 +1,4 @@
+import { style } from 'framer-motion/client'
 import styles from './Carrito.module.css'
 import { motion } from 'framer-motion'
 
@@ -6,24 +7,55 @@ interface Props {
     mostrarCarrito: boolean
 }
 
-export const Carrito = ({ productos, mostrarCarrito}: Props) => {
+export const Carrito = ({ productos, mostrarCarrito }: Props) => {
+    
+
+    const productosTotal = (() => {
+        let total = 0
+        productos.forEach(producto => {
+            total += producto.price
+        })
+        return total
+    })
+
     return (
         <motion.div
             initial={{ width: 0 }}
-            animate={{ width: mostrarCarrito ? '20rem' : 0 }}
+            animate={{ width: mostrarCarrito ? '25rem' : '0px' }}
             transition={{ duration: 0.5 }}
             className={styles.contenedor}
         >
-            {
-                productos.map((producto, index) => {
-                    return (    
-                        <div key={index}>
-                            <h3>{producto.title}</h3>
-                            <p>{producto.price}</p>
+            <div className={styles.todoslosproductos}>
+                   {
+                    productos.length === 0 ? 
+                        <div className={styles.nohayproductos}>
+                            NO HAY PRODUCTOS EN EL CARRITO
                         </div>
-                    )
-                })
+                        :
+                    productos.map((producto, index) => {
+                        return (    
+                            <div key={index} className={styles.contenedorProducto}>
+                                <div className={styles.contenedorImagen}>
+                                    <img src={producto.image} className={styles.imagen} />
+                                </div>
+                                <div className={styles.informacion}>
+                                    <h3 className={styles.titulo}>{producto.title}</h3>
+                                    <p className={styles.precio}>${producto.price}</p>
+                                </div>
+                            </div>
+                            
+                        )
+                    })
+                    }
+            </div>
+            {
+                productos.length > 0 &&
+                <div className={styles.contenedorPagar}>
+                    <p>${productosTotal()}</p>
+                    <button className={styles.botonPagar}>PAGAR</button>
+                </div>
             }
+            
         </motion.div>
     )
 }
